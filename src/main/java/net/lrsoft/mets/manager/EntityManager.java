1af29ad308c8ec64fb2adcf3bbacc93dabec6d30
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-08-17 04:47:30 +0800

    Update EntityManager.java

--- src/main/java/net/lrsoft/mets/manager/EntityManager.java
+++ src/main/java/net/lrsoft/mets/manager/EntityManager.java
@@ -4,7 +4,6 @@ import net.lrsoft.mets.MoreElectricTools;
 import net.lrsoft.mets.blade.EntityDriveEx;
 import net.lrsoft.mets.blade.EntitySlashDimensionEx;
 import net.lrsoft.mets.blade.EntitySpearManagerEx;
-import net.lrsoft.mets.entity.*;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.RegistryEvent;
@@ -16,32 +15,25 @@ import net.minecraftforge.fml.common.registry.EntityEntryBuilder;
 
 @Mod.EventBusSubscriber(modid = MoreElectricTools.MODID)
 public class EntityManager {
-	 private static int currentEntityId = 0;
-	@SubscribeEvent
-	public static void onEntityInit(RegistryEvent.Register<EntityEntry> event)
-	{
-		registerEntity(event, EntityGunBullet.class, "GunBullet", 200, 5);
-		registerEntity(event, EntityHyperGunBullet.class, "HyperGunBullet", 250, 5);
-		registerEntity(event, EntityRocket.class, "RocketBullet", 200, 3);
-		registerEntity(event, EntityPlasmaBullet.class, "PlasmaBullet", 200, 5);
-		
-		if (Loader.isModLoaded("flammpfeil.slashblade")) 
-		{
-			registerEntity(event, EntitySlashDimensionEx.class, "EntitySlashDimensionEx", 400, 1);
-			registerEntity(event, EntityDriveEx.class, "EntityDriveEx", 400, 1);
-			registerEntity(event, EntitySpearManagerEx.class, "EntitySpearManagerEx", 400, 1);
-		}
-	}
-	
-	private static void registerEntity(RegistryEvent.Register<EntityEntry> event, 
-			Class<? extends Entity> entityClass, String entityName, int updateRange, int updateFrequency)
-	{
-	    event.getRegistry().register(EntityEntryBuilder.create()
-	            .entity(entityClass)
-	            .id(new ResourceLocation(MoreElectricTools.MODID, entityName), currentEntityId++)
-	            .name(entityName)
-	            .tracker(updateRange, updateFrequency, true)
-	            .build()
-	    );
-	}
+    private static int currentEntityId = 0;
+    
+    @SubscribeEvent
+    public static void onEntityInit(RegistryEvent.Register<EntityEntry> event) {
+        if (Loader.isModLoaded("flammpfeil.slashblade")) {
+            registerEntity(event, EntitySlashDimensionEx.class, "EntitySlashDimensionEx", 400, 1);
+            registerEntity(event, EntityDriveEx.class, "EntityDriveEx", 400, 1);
+            registerEntity(event, EntitySpearManagerEx.class, "EntitySpearManagerEx", 400, 1);
+        }
+    }
+    
+    private static void registerEntity(RegistryEvent.Register<EntityEntry> event, 
+            Class<? extends Entity> entityClass, String entityName, int updateRange, int updateFrequency) {
+        event.getRegistry().register(EntityEntryBuilder.create()
+                .entity(entityClass)
+                .id(new ResourceLocation(MoreElectricTools.MODID, entityName), currentEntityId++)
+                .name(entityName)
+                .tracker(updateRange, updateFrequency, true)
+                .build()
+        );
+    }
 }

dbf7c1805e83b9e7186a7c139afe7a8c97ecca99
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-08-17 03:36:47 +0800

    Update ModelManager.java

--- src/main/java/net/lrsoft/mets/manager/ModelManager.java
+++ src/main/java/net/lrsoft/mets/manager/ModelManager.java
@@ -17,6 +17,7 @@ import net.lrsoft.mets.item.blade.BladeModelManager;
 import net.lrsoft.mets.item.crafting.ItemCraftingManager;
 import net.lrsoft.mets.renderer.BulletRenderer;
 import net.lrsoft.mets.renderer.LighterRenderer;
+import net.lrsoft.mets.renderer.NanoShieldRenderer;
 import net.lrsoft.mets.renderer.PlasmaBulletRenderer;
 import net.lrsoft.mets.renderer.RocketRender;
 import net.lrsoft.mets.renderer.TransmissionNodeRenderer;
@@ -64,10 +65,10 @@ public class ModelManager {
 				new ModelResourceLocation(ItemManager.lithiumBattery.getRegistryName(), "inventory"));
 		ModelLoader.setCustomModelResourceLocation(ItemManager.thoriumBattery, 0,
 				new ModelResourceLocation(ItemManager.thoriumBattery.getRegistryName(), "inventory"));
-		ModelLoader.setCustomModelResourceLocation(ItemManager.electricFishingRod, 0,
-				new ModelResourceLocation(ItemManager.electricFishingRod.getRegistryName(), "inventory"));
-		// ModelLoader.setCustomModelResourceLocation(ItemManager.electricShield, 0,
-		//         new ModelResourceLocation(ItemManager.electricShield.getRegistryName(), "inventory"));  // 删除这行
+		// ModelLoader.setCustomModelResourceLocation(ItemManager.electricFishingRod, 0,
+		//         new ModelResourceLocation(ItemManager.electricFishingRod.getRegistryName(), "inventory"));  // 删除这行
+		ModelLoader.setCustomModelResourceLocation(ItemManager.electricShield, 0,
+				new ModelResourceLocation(ItemManager.electricShield.getRegistryName(), "inventory"));
 		ModelLoader.setCustomModelResourceLocation(ItemManager.electricLighter, 0,
 				new ModelResourceLocation(ItemManager.electricLighter.getRegistryName(), "inventory"));
 		ModelLoader.setCustomModelResourceLocation(ItemManager.electricWirlessManager, 0,
@@ -86,7 +87,7 @@ public class ModelManager {
 		ModelLoader.setCustomModelResourceLocation(ItemManager.tacticalLaserSubmachineGun, 0,
 				new ModelResourceLocation(ItemManager.tacticalLaserSubmachineGun.getRegistryName(), "inventory"));
 		
-		// ItemManager.electricShield.setTileEntityItemStackRenderer(new net.lrsoft.mets.renderer.NanoShieldRenderer());  // 删除这行
+		ItemManager.electricShield.setTileEntityItemStackRenderer(new NanoShieldRenderer());
 		
 		ItemCraftingManager.onCraftingItemModelInit();
 
@@ -110,6 +111,7 @@ public class ModelManager {
 		ClientRegistry.bindTileEntitySpecialRenderer(TileEntityLighterBlock.class, new LighterRenderer());
 		//ClientRegistry.bindTileEntitySpecialRenderer(TileEntityWirelessPowerTransmissionNode.class, new TransmissionNodeRenderer());
 	}
+	
 	@SubscribeEvent
 	public static void onFluidModelInit(ModelRegistryEvent event)
 	{

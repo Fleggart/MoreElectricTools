1ee6f7647c9a2fdf11ed351c3d794f4e9d3a2040
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-08-17 04:28:38 +0800

    Refactor SoundManager to remove laser_bullet_shoot
    Removed unused sound event and related registration.

--- src/main/java/net/lrsoft/mets/manager/SoundManager.java
+++ src/main/java/net/lrsoft/mets/manager/SoundManager.java
@@ -9,14 +9,10 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Mod.EventBusSubscriber(modid = MoreElectricTools.MODID)
 public class SoundManager {
-	public static SoundEvent laser_bullet_shoot = new SoundEvent(new ResourceLocation(MoreElectricTools.MODID, "laser_bullet_shoot"));
-	public static SoundEvent lighter_place = new SoundEvent(new ResourceLocation(MoreElectricTools.MODID, "lighter_place"));
-	
-	@SubscribeEvent
-	public static void onSoundEventRegistration(RegistryEvent.Register<SoundEvent> event) 
-	{
-	    event.getRegistry().register(laser_bullet_shoot.setRegistryName("laser_bullet_shoot"));
-	    event.getRegistry().register(lighter_place.setRegistryName("lighter_place"));
-	}
-	
+    public static SoundEvent lighter_place = new SoundEvent(new ResourceLocation(MoreElectricTools.MODID, "lighter_place"));
+    
+    @SubscribeEvent
+    public static void onSoundEventRegistration(RegistryEvent.Register<SoundEvent> event) {
+        event.getRegistry().register(lighter_place.setRegistryName("lighter_place"));
+    }
 }

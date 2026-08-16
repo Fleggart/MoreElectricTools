740bdaa0a87e13e29014ff4958fb8be42ad33721
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-08-17 04:42:05 +0800

    Update TileEntiyLaserTower.java

--- src/main/java/net/lrsoft/mets/block/tileentity/TileEntiyLaserTower.java
+++ src/main/java/net/lrsoft/mets/block/tileentity/TileEntiyLaserTower.java
@@ -9,7 +9,6 @@ import com.google.common.base.Predicates;
 
 import mods.flammpfeil.slashblade.SlashBlade;
 import net.lrsoft.mets.block.tileentity.OilRig.TileEntityGUIMachine;
-import net.lrsoft.mets.entity.EntityGunBullet;
 import net.lrsoft.mets.manager.ConfigManager;
 import net.lrsoft.mets.manager.SoundManager;
 import net.minecraft.entity.Entity;
@@ -28,87 +27,86 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 
 public class TileEntiyLaserTower extends TileEntityGUIMachine {
-	private static double attackConsume = ConfigManager.LaserTowerCost;
-	private Vec3d shootOffset = new Vec3d(0.5d, 1.0d, 0.5d);
-	private static int maxEnergy = 100000, tier = 2;
-	
-	private int scanRange = 15;
-	
-	private int updateInterval = 45, currentInterval = 0;
-	public TileEntiyLaserTower() {
-		super(maxEnergy, tier);
-	}
-	
-	@Override
-	protected void updateEntityServer() {
-		super.updateEntityServer();
-		updateTileEntity();
-	}
-	
-	private void updateTileEntity() {
-		boolean shouldActive = false;
-		if (this.energy.canUseEnergy(attackConsume)) {
-			shouldActive = true;
-			
-			currentInterval++;
-			if(currentInterval >= updateInterval) {
-				Vec3d currentPos = new Vec3d(this.pos.getX(), pos.getY(), pos.getZ());
-				currentPos = currentPos.add(shootOffset);
+    private static double attackConsume = ConfigManager.LaserTowerCost;
+    private Vec3d shootOffset = new Vec3d(0.5d, 1.0d, 0.5d);
+    private static int maxEnergy = 100000, tier = 2;
+    
+    private int scanRange = 15;
+    
+    private int updateInterval = 45, currentInterval = 0;
+    public TileEntiyLaserTower() {
+        super(maxEnergy, tier);
+    }
+    
+    @Override
+    protected void updateEntityServer() {
+        super.updateEntityServer();
+        updateTileEntity();
+    }
+    
+    private void updateTileEntity() {
+        boolean shouldActive = false;
+        if (this.energy.canUseEnergy(attackConsume)) {
+            shouldActive = true;
+            
+            currentInterval++;
+            if (currentInterval >= updateInterval) {
+                Vec3d currentPos = new Vec3d(this.pos.getX(), pos.getY(), pos.getZ());
+                currentPos = currentPos.add(shootOffset);
 
-				AxisAlignedBB bb = new AxisAlignedBB(
-						currentPos.x - scanRange, currentPos.y - scanRange, currentPos.z - scanRange, 
-						currentPos.x + scanRange, currentPos.y + scanRange, currentPos.z + scanRange);
+                AxisAlignedBB bb = new AxisAlignedBB(
+                        currentPos.x - scanRange, currentPos.y - scanRange, currentPos.z - scanRange, 
+                        currentPos.x + scanRange, currentPos.y + scanRange, currentPos.z + scanRange);
 
-				List<Entity> list = this.world.getEntitiesInAABBexcluding(null, bb, ALIVE_MOB_SELECTOR);
-				if (!list.isEmpty() && this.energy.useEnergy(attackConsume)) {
-					Entity mob = list.get(0);
+                List<Entity> list = this.world.getEntitiesInAABBexcluding(null, bb, ALIVE_MOB_SELECTOR);
+                if (!list.isEmpty() && this.energy.useEnergy(attackConsume)) {
+                    Entity mob = list.get(0);
 
-					double yOffset = mob.getEyeHeight();
-					Vec3d mobPos = new Vec3d(mob.posX, mob.posY + yOffset, mob.posZ);
-					mobPos = mobPos.subtract(currentPos);
-					
-					double vecLength = mobPos.lengthVector();
-					double tVecLength = 1.0D - vecLength;
-					tVecLength = tVecLength * tVecLength;
-					
-					Vec3d motion = new Vec3d(mobPos.x / vecLength * tVecLength, mobPos.y / vecLength * tVecLength, mobPos.z / vecLength * tVecLength);
-					
-					EntityGunBullet bullet = new EntityGunBullet(world, currentPos, 12f, 360, true);
-					bullet.shoot(motion.x, motion.y, motion.z, 3f, 0.0f);
-					
-					world.spawnEntity(bullet);	
-					
-					world.playSound((EntityPlayer)null, currentPos.x , currentPos.y, currentPos.z, 
-							SoundManager.laser_bullet_shoot, SoundCategory.AMBIENT, 0.35f, 0.1F);
-				}
-				
-				currentInterval = 0;
-			}
-		}else {
-			currentInterval = 0;
-		}
-		setActive(shouldActive);
-	}
-	
-	@Override
-	public NBTTagCompound writeToNBT(NBTTagCompound tag) {
-		super.writeToNBT(tag);
-		tag.setInteger("shootInterval", currentInterval);
-		return tag;
-	}
-	
-	@Override
-	public void readFromNBT(NBTTagCompound tag) {
-		super.readFromNBT(tag);
-		currentInterval = tag.getInteger("shootInterval");
-	}
-	
-    public static Predicate<Entity> ALIVE_MOB_SELECTOR = new Predicate<Entity>()
-    {
-        public boolean apply(@Nullable Entity entity)
-        {
+                    double yOffset = mob.getEyeHeight();
+                    Vec3d mobPos = new Vec3d(mob.posX, mob.posY + yOffset, mob.posZ);
+                    mobPos = mobPos.subtract(currentPos);
+                    
+                    double vecLength = mobPos.lengthVector();
+                    double tVecLength = 1.0D - vecLength;
+                    tVecLength = tVecLength * tVecLength;
+                    
+                    Vec3d motion = new Vec3d(mobPos.x / vecLength * tVecLength, mobPos.y / vecLength * tVecLength, mobPos.z / vecLength * tVecLength);
+                    
+                    // TODO: 激光塔功能需要重新实现 - EntityGunBullet 已被移除
+                    // 原代码使用 EntityGunBullet 发射子弹
+                    // 建议: 使用 EntityPlasmaBullet 或自定义激光实体
+                    
+                    // 暂时使用原版火焰弹效果
+                    // world.spawnEntity(new EntitySmallFireball(world, currentPos.x, currentPos.y, currentPos.z, motion.x, motion.y, motion.z));
+                    
+                    world.playSound((EntityPlayer)null, currentPos.x, currentPos.y, currentPos.z, 
+                            SoundManager.lighter_place, SoundCategory.AMBIENT, 0.35f, 0.1F);
+                }
+                
+                currentInterval = 0;
+            }
+        } else {
+            currentInterval = 0;
+        }
+        setActive(shouldActive);
+    }
+    
+    @Override
+    public NBTTagCompound writeToNBT(NBTTagCompound tag) {
+        super.writeToNBT(tag);
+        tag.setInteger("shootInterval", currentInterval);
+        return tag;
+    }
+    
+    @Override
+    public void readFromNBT(NBTTagCompound tag) {
+        super.readFromNBT(tag);
+        currentInterval = tag.getInteger("shootInterval");
+    }
+    
+    public static Predicate<Entity> ALIVE_MOB_SELECTOR = new Predicate<Entity>() {
+        public boolean apply(@Nullable Entity entity) {
             return entity != null && entity instanceof IMob && entity.isEntityAlive();
         }
     };
-
 }

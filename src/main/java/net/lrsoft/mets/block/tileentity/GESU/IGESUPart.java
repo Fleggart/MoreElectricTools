edd00149499cf28ffeb54bf5a41dcdd06d9d7633
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-08-17 12:37:40 +0800

    Delete src/main/java/net/lrsoft/mets/block/tileentity/GESU directory

--- src/main/java/net/lrsoft/mets/block/tileentity/GESU/IGESUPart.java
+++
@@ -1,8 +0,0 @@
-package net.lrsoft.mets.block.tileentity.GESU;
-
-import net.minecraft.util.math.Vec3d;
-
-public interface IGESUPart {
-	void setCore(Vec3d coord);
-	void setCoreComplete(boolean isComplete);
-}
--- src/main/java/net/lrsoft/mets/block/tileentity/GESU/TileEntityGESULuVOutputPort.java
+++
@@ -1,8 +0,0 @@
-package net.lrsoft.mets.block.tileentity.GESU;
-
-public class TileEntityGESULuVOutputPort extends TileEntityGESUOutputPort {
-	public TileEntityGESULuVOutputPort()
-	{
-		super(6);
-	}
-}

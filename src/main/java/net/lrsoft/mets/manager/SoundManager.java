c0e2b2f240522fde9fdacddbe59e9586d2bb7c1a
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-08-17 08:11:52 +0800

    Update SoundManager.java

--- src/main/java/net/lrsoft/mets/manager/SoundManager.java
+++ src/main/java/net/lrsoft/mets/manager/SoundManager.java
@@ -9,10 +9,11 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Mod.EventBusSubscriber(modid = MoreElectricTools.MODID)
 public class SoundManager {
-    public static SoundEvent lighter_place = new SoundEvent(new ResourceLocation(MoreElectricTools.MODID, "lighter_place"));
-    
+    // ========== 已移除 ==========
+    // public static SoundEvent lighter_place = new SoundEvent(new ResourceLocation(MoreElectricTools.MODID, "lighter_place"));
+
     @SubscribeEvent
     public static void onSoundEventRegistration(RegistryEvent.Register<SoundEvent> event) {
-        event.getRegistry().register(lighter_place.setRegistryName("lighter_place"));
+        // event.getRegistry().register(lighter_place.setRegistryName("lighter_place"));  // 已移除
     }
 }
